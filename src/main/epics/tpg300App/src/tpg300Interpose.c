601934b42d5adf00ac3686416f30955931903c27
Klemen Strnisa <klemen.strnisa@esss.se>
2015-05-31 16:12:20 +0200

    Added Licence info.

--- src/main/epics/tpg300App/src/tpg300Interpose.c
+++ src/main/epics/tpg300App/src/tpg300Interpose.c
@@ -1,4 +1,20 @@
-/* tpg300Interpose.c */
+/**
+ * Pfeiffer Vacuum TPG300 EPICS module
+ * Copyright (C) 2015 Cosylab
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
  * Interpose layer for EPICS support for the Pfeiffer Vacuum TPG300 controller
